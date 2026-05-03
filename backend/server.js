69aaba2d2c00ec9bff9aa02ab65b4fe257783867
Imesh Bandara <imeshnbandara826@gmail.com>
2026-05-03 23:48:17 +0530

    created server.js file

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // json daththa huwamaru krann use kranne
+
+//mongodb connect kirima
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("VisitCeylonX Database Connected Successfully! ✅"))
+.catch(err=>console.log("DB connection error :", err));
+
+//basic route
+app.get('/' , (req,res)=>{
+    res.send("VisitCeyloneX Backend API is running...");
+});
+
+//port setting
+const PORT = process.env.PORT || 5002;
+app.listen(PORT , ()=>{
+    console.log('server is running on port ${PORT}');
+});
+
+//username = visitslone
+//password = passwordekamokakda
+//mongodb+srv://visitslone:passwordekamokakda@cluster0.pf9ql4o.mongodb.net/?appName=Cluster0
