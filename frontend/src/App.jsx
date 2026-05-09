7e3730732d7d208b51097ec3aa335482b84f58bf
Imesh Bandara <imeshnbandara826@gmail.com>
2026-05-09 13:56:40 +0530

    modified app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
+import GuidePage from './pages/GuidePage';
 
 function App() {
   return (
-    <div className="min-h-screen font-poppins">
-      <Navbar />
-      <HomePage />
-      {/* passe methaat footer ekak ekathu kranna puluwn */}
-    </div>
+    <Router>
+      <div className="min-h-screen font-poppins bg-background">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/guides" element={<GuidePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
