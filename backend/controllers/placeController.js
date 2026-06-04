155603e68c938c9c4d2168d199b8b7e8611ee8dd
Imesh Bandara <imeshnbandara826@gmail.com>
2026-06-04 23:20:47 +0530

    created placeController.js

---
+++ backend/controllers/placeController.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import Place from '../models/Place.js';
+
+// GET single place data from database
+export const getPlaceById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Check if the ID provided is a valid MongoDB ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid Database Identifier Format"
+      });
+    }
+
+    const place = await Place.findById(id);
+
+    if (!place) {
+      return res.status(404).json({
+        message: "Destination details not found in database"
+      });
+    }
+
+    // Return data back to React frontend
+    return res.status(200).json(place);
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Database link dropped",
+      error: error.message
+    });
+  }
+};
