86a143337d8dbd82594ecd5648e7bff924ea9d31
Imesh Bandara <imeshnbandara826@gmail.com>
2026-06-08 18:28:46 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,6 +42,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/all-places" element={<AllPlaces />} />
           <Route path="/event/:id" element={<EventDetails />} />
+          <Route path="/weather-guide" element={<WeatherGuide />} />
           <Route 
               path="/planner" 
               element={
