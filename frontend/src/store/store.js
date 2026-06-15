b4488cc89b5a6ecdf2fc5d877cc37507dc7cd5be
Imesh Bandara <imeshnbandara826@gmail.com>
2026-06-16 01:08:07 +0530

    Register the Events Reducer in store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './weatherSlice.js';
 import placesReducer from './placesSlice.js';
 import authReducer from './authSlice.js';
+import eventsReducer from './eventsSlice.js';
 
 export const store = configureStore({
   reducer: {
     weather: weatherReducer,
     places: placesReducer,
-    auth: authReducer // passe placeReducer, eventReducer unah methnt ekathu karanna puluwn
+    auth: authReducer,// passe placeReducer, eventReducer unah methnt ekathu karanna puluwn
+    events: eventsReducer,
   },
 });
