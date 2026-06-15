d5b64608ab3e6e50c622cce6c0e8930c38ddbf0d
Imesh Bandara <imeshnbandara826@gmail.com>
2026-06-15 15:07:25 +0530

    updated event.js

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -3,10 +3,27 @@ import mongoose from 'mongoose';
 const eventSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  month: { type: String, required: true }, // e.g., "August"
-  location: { type: String, required: true }, // e.g., "Kandy"
-  image: { type: String, required: true }, // Public folder path or URL
+  month: { type: String, required: true }, 
+  location: { type: String, required: true }, 
+  image: { type: String, required: true }, 
 }, { timestamps: true });
+
+/
+eventSchema.pre('save', async function() {
+  if (this.image && this.image.startsWith('/public')) {
+    this.image = this.image.replace('/public', '');
+  }
+});
+
+
+eventSchema.pre('findOneAndUpdate', async function() {
+  const update = this.getUpdate();
+  const data = update.$set || update;
+  
+  if (data.image && data.image.startsWith('/public')) {
+    data.image = data.image.replace('/public', '');
+  }
+});
 
 const Event = mongoose.model('Event', eventSchema);
 export default Event;
