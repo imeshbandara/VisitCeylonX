a342eeef5175b4ed475b91afe7987bac00d8191c
Imesh Bandara <imeshnbandara826@gmail.com>
2026-06-15 19:13:58 +0530

    imported redux to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // Imports your Tailwind styles
+import { Provider } from 'react-redux'; // redux imports
+import { store } from './store/store.js'; // redux imports
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
