6d09458eaead3b2cccf7085317aecf645047ad02
Imesh Bandara <imeshnbandara826@gmail.com>
2026-06-15 23:58:56 +0530

    imported placeslice to store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './weatherSlice.js';
+import placesReducer from './placesSlice.js';
 
 export const store = configureStore({
   reducer: {
-    weather: weatherReducer, // passe placeReducer, eventReducer unah methnt ekathu karanna puluwn
+    weather: weatherReducer,
+    places: placesReducer, // passe placeReducer, eventReducer unah methnt ekathu karanna puluwn
   },
 });
